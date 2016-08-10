b180572e332bd16e6b669f48e7a81fd09dd310fa
Sriharsha Gundappa <harshag7@gmail.com>
2016-08-09 20:40:20 -0400

    Break Test cases

---
+++ clonedetector/src/test/java/newtest/BreakTest1.java
@@ -0,0 +1,27 @@
+package newtest;
+
+import newtest.dto.Person;
+
+public class BreakTest1 {
+	
+	void createPerson1() {
+		Person p = new Person();
+		p.setId(1);
+		p.setName("there");
+		p.setPhone("123-456-7890");
+	}
+	
+	void createPerson2() {
+		Person p = new Person();
+		p.setId(2);
+		p.setName("hithe");
+		p.setPhone("123-456-7890");
+	}
+
+	public static void main(String[] args) {
+		BreakTest1 test = new BreakTest1();
+		test.createPerson1();
+		test.createPerson2();
+	}
+
+}
---
+++ clonedetector/src/test/java/newtest/BreakTest2.java
@@ -0,0 +1,11 @@
+package newtest;
+
+public class BreakTest2 extends BreakTest1 {
+
+	public static void main(String[] args) {
+		BreakTest1 test = new BreakTest2();
+		test.createPerson1();
+		test.createPerson2();
+	}
+
+}
