13a7a3a8acac55b3d85b2f0a7efa328bab0c118c
Sriharsha Gundappa <harshag7@gmail.com>
2016-08-10 20:27:00 -0400

    BreakTest3 fails step 2

--- clonedetector/src/test/java/newtest/BreakTest2.java
+++ clonedetector/src/test/java/newtest/BreakTest2.java
@@ -1,8 +1,15 @@
 package newtest;
 
-public class BreakTest2 extends BreakTest1 {
+public class BreakTest2 extends BreakTest1 implements Runnable {
 
 	public static void main(String[] args) {
+		BreakTest1 test = new BreakTest2();
+		test.createPerson1();
+		test.createPerson2();
+	}
+
+	@Override
+	public void run() {
 		BreakTest1 test = new BreakTest2();
 		test.createPerson1();
 		test.createPerson2();
---
+++ clonedetector/src/test/java/newtest/BreakTest3.java
@@ -0,0 +1,66 @@
+package newtest;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import newtest.dto.Person;
+
+public class BreakTest3 {
+	
+	static ExecutorService service = Executors.newFixedThreadPool(10);
+	
+	public static int createThread1() {
+		
+		int count = 0;
+		for(int i=0;i<5;i++) {
+			count ++;
+			Runnable worker = new Runnable() {
+				
+				@Override
+				public void run() {
+					Person p1 = new Person();
+					p1.setId(5);
+					System.out.println(Math.random());
+				}
+			};
+			service.execute(worker);
+		}
+		return count;
+	}
+	
+	public static int createThread2() {
+		int count = 0;
+		for(int i=0;i<5;i++) {
+			count ++;
+			Runnable worker = new Runnable() {
+				
+				@Override
+				public void run() {
+					Person p1 = new Person();
+					p1.setName("Hello");
+					System.out.println(Math.random() + p1.getId());
+				}
+			};
+			service.execute(worker);
+		}
+		return count;
+	}
+	
+	public static int createThread3() {
+		int count = 0;
+		for(int i=0;i<5;i++) {
+			count ++;
+			service.execute(new BreakTest2());
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		
+		createThread1();
+		createThread2();
+		createThread3();
+
+	}
+
+}
